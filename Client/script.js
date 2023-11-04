1cb865b57e41e5b2bd75bb07f439fcfb39a49c01
Index.html <gmanley@uwo.ca>
2023-11-04 11:41:44 -0400

    addition of list.json writing

--- Client/script.js
+++ Client/script.js
@@ -6,17 +6,206 @@ const searchLimit = document.getElementById('searchLimit');
 const searchBtn = document.getElementById('searchBtn');
 const results = document.getElementById('result');
 const searchResult = document.getElementById('searchResult');
+const publishersBtn = document.getElementById('publishersBtn');
+const selectListBtn = document.getElementById('selectListBtn');
+const listDrop = document.getElementById('listDrop');
+const listName = document.getElementById('listName');
+const saveListBtn = document.getElementById('saveListBtn');
+const newListBtn = document.getElementById('newListBtn')
+const deleteListBtn = document.getElementById('deleteListBtn');
+const sortListDrop = document.getElementById('sortList');
+const sortListBtn = document.getElementById('sortListBtn');
 
 searchBtn.addEventListener('click', getHeroes);
+publishersBtn.addEventListener('click', getPublishers);
+selectListBtn.addEventListener('click', getList);
+saveListBtn.addEventListener('click', saveList);
+newListBtn.addEventListener('click', createList);
+deleteListBtn.addEventListener('click', deleteList);
+sortListBtn.addEventListener('click', sortList);
+sortListDrop.addEventListener('change', () => {
+    sortListBtn.disabled = sortListDrop.selectedIndex === 0;
+});
+listDrop.addEventListener('change', e => {
+    selectListBtn.disabled = listDrop.selectedIndex === 0
+    listName.value = e.target.value
+});
+searchLimit.addEventListener('input', e => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, '');
+    if (e.target.value.length > 4) {
+        e.target.value = e.target.value.slice(0, 4);
+    }
+})
 
-// searchRace.addEventListener('input', getRace);
+populateDropdown();
+selectListBtn.disabled = true;
+sortListBtn.disabled = true;
 
-// searchPublisher.addEventListener('input', getPublisher);
+function sortList() {
+    const sortType = sortListDrop.options[sortListDrop.selectedIndex].value;
+    const listItems = Array.from(searchResult.getElementsByTagName('li'));
+
+    listItems.sort((a, b) => {
+        let textA, textB;
+
+        if (sortType === 'name') {
+            textA = a.querySelector('h1.name').textContent.trim().toLowerCase();
+            textB = b.querySelector('h1.name').textContent.trim().toLowerCase();
+        } else {
+            textA = a.querySelector(`p[class="${sortType}"]`).textContent.trim().toLowerCase();
+            textB = b.querySelector(`p[class="${sortType}"]`).textContent.trim().toLowerCase();
+        }
+
+
+        return textA.localeCompare(textB);
+    });
+
+    searchResult.innerHTML = '';
+    listItems.forEach(item => searchResult.appendChild(item));
+}
+
+function saveList() {
+    const idList = [];
+    for(hero of searchResult.getElementsByTagName('li')) {
+        idList.push(hero.className);
+    }
+    const newList = { 
+        lName: listName.value, 
+        id: idList
+    }
+
+    fetch(`/api/superheroes/lists/${listDrop.selectedIndex}`, {
+        method: 'PUT',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(newList)
+    })
+    .then(res => {
+        if(res.status === 400) throw new Error('List name cannot be blank...');
+        if(res.status === 404) throw new Error('List not found...');
+        res.json();
+    })
+    .then(data => {
+        populateDropdown();
+    })
+    .catch(err => console.log(err.message));
+}
+
+function createList() {
+    const idList = [];
+    for(hero of searchResult.getElementsByTagName('li')) {
+        idList.push(hero.className);
+    }
+    const newList = { 
+        lName: listName.value, 
+        id: idList
+    }   
+
+    fetch('/api/superheroes/lists', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(newList)
+    })
+    .then(res => {
+        if(res.status === 400) throw new Error('List name cannot be blank...')
+        if(res.status === 409) throw new Error('List name taken...');
+        res.json()
+    })
+    .then(data => {
+        populateDropdown();
+    })
+    .catch(err => console.log(err.message));    
+}
+
+function deleteList() {
+    fetch(`/api/superheroes/lists/${listDrop.selectedIndex}`, {
+        method: 'DELETE',
+        headers: {'Content-type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(data => {
+        listName.value = '';
+        listDrop.remove(listDrop.selectedIndex);
+        listDrop.options[0].selected = true;
+        sortListDrop[0].selected = true;
+    })
+
+}
+
+function populateDropdown() {
+    fetch('/api/superheroes/lists')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(list => {
+            if(!optionExists(listDrop, list.lName) || listDrop.options.length === 1) {
+                let option = document.createElement("option");
+                option.text = list.lName;
+                option.value = list.lName;
+                listDrop.appendChild(option);   
+            }
+        });
+        clearResults();
+        listName.value = '';
+        newListBtn.disabled = true;
+        saveListBtn.disabled = true;
+        sortListDrop.disabled = true;
+    });
+}
+
+function getList() {
+    clearResults();
+    fetch(`/api/superheroes/lists/${listDrop.selectedIndex}`)
+    .then(res => res.json())
+    .then(data => {
+        for(id of data.id) {
+            getHero(id);
+        }
+        newListBtn.disabled = false;
+        saveListBtn.disabled = false;
+        sortListDrop.disabled = false;
+    });
+    
+}
+
+function getHero(id) {
+    fetch(`/api/superheroes/${id}`)
+    .then(res => res.json())
+    .then(data => {
+        const heroLI = document.createElement('li');
+        for(e in data) {
+            if(e === 'id') continue;
+            if(e === 'name') {
+                const nameH1 = document.createElement('h1');
+                nameH1.textContent = capitalize(data[e]);
+                heroLI.appendChild(nameH1);
+            } else if(e === 'Race') {
+                const raceP = document.createElement('p');
+                raceP.textContent = capitalize(`Race: ${data[e]}`);
+                raceP.className = 'race';
+                heroLI.appendChild(raceP);
+            } else if(e === 'Publisher') {
+                const publisherP = document.createElement('p');
+                publisherP.textContent = capitalize(`Publisher: ${data[e]}`);
+                publisherP.className = 'publisher';
+                heroLI.appendChild(publisherP);
+            } else {
+                heroLI.appendChild(document.createTextNode(`${capitalize(e)}: ${capitalize(data[e])}`));
+                heroLI.appendChild(document.createElement('br'));
+            }
+        }
+        getPower(data.name, heroLI);
+        searchResult.appendChild(heroLI);
+    })
+}
 
 function getHeroes() {
     clearResults();
-    fetch(`/api/superheroes/filter?name=${searchName.value}&race=${searchRace.value}&publisher=${searchPublisher.value}&limit=${searchLimit.value}`)
+    fetch(`/api/superheroes/filter?name=${searchName.value}&race=${searchRace.value}&publisher=${searchPublisher.value}&power=${searchPower.value}&limit=${searchLimit.value}`)
     .then(res => {
+        if(!res.ok) {
+            newListBtn.disabled = true;
+            saveListBtn.disabled = true;
+            sortListDrop.disabled = true;
+        }
         if(res.status === 400) throw new Error('There must be at least one keyword...')
         if(res.status === 404) throw new Error('No matches...')
         return res.json();
@@ -25,19 +214,33 @@ function getHeroes() {
         data.forEach(hero => {
             const heroLI = document.createElement('li');
             for(e in hero) {
-                if(e === 'id') continue;
-                if(e === 'name') {
+                if(e === 'id') heroLI.className = hero[e];
+                else if(e === 'name') {
                     const nameH1 = document.createElement('h1');
                     nameH1.textContent = capitalize(hero[e]);
+                    nameH1.className = 'name';
                     heroLI.appendChild(nameH1);
-                    continue;
+                } else if(e === 'Race') {
+                    const raceP = document.createElement('p');
+                    raceP.textContent = capitalize(`Race: ${hero[e]}`);
+                    raceP.className = 'race';
+                    heroLI.appendChild(raceP);
+                } else if(e === 'Publisher') {
+                    const publisherP = document.createElement('p');
+                    publisherP.textContent = capitalize(`Publisher: ${hero[e]}`);
+                    publisherP.className = 'publisher';
+                    heroLI.appendChild(publisherP);
+                } else {
+                    heroLI.appendChild(document.createTextNode(`${capitalize(e)}: ${capitalize(hero[e])}`));
+                    heroLI.appendChild(document.createElement('br'));
                 }
-                heroLI.appendChild(document.createTextNode(`${capitalize(e)}: ${capitalize(hero[e])}`));
-                heroLI.appendChild(document.createElement('br'));
             }
             getPower(hero.name, heroLI);
             searchResult.appendChild(heroLI);
-        })
+        });
+        newListBtn.disabled = false;
+        saveListBtn.disabled = false;
+        sortListDrop.disabled = false;
     })
     .catch(err => {
         console.log(err.message);
@@ -66,6 +269,31 @@ function getPower(heroName, li) {
     });
 }
 
+function getPublishers() {
+    clearResults();
+    fetch('/api/superheroes/publishers')
+    .then(res => res.json())
+    .then(data => {
+        const publishersLI = document.createElement('li');
+        const publishersH1 = document.createElement('h1');
+        publishersH1.textContent = 'Publishers';
+        publishersLI.appendChild(publishersH1);
+        for(const p in data) {
+            publishersLI.appendChild(document.createTextNode(`${data[p]}, `));
+        }
+        searchResult.appendChild(publishersLI);
+    })
+}
+
+function optionExists(dropdown, value) {
+    for (let i = 0; i < dropdown.options.length; i++) {
+        if (dropdown.options[i].value === value) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function capitalize(phrase) {
     phrase = String(phrase).split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
     return phrase;
@@ -90,8 +318,16 @@ searchPublisher.addEventListener('keydown', e => {
         searchBtn.click();
     }
 });
+searchPower.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        searchBtn.click();
+    }
+});
 searchLimit.addEventListener('keydown', e => {
     if(e.key === 'Enter') {
         searchBtn.click();
     }
 });
+listName.addEventListener('keydown', e => {
+    if(e.key === 'Enter') newListBtn.click()
+})
--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -7,25 +8,32 @@ const routerPower = express.Router();
 const port = 3000;
 const superInfo = 'server/superhero_info.json';
 const superPowers = 'server/superhero_powers.json';
-const lists = 'server/list.json';
+const lists = 'server/lists.json';
 let superInfoJSON;
 let superPowersJSON;
+let listsJSON
 
 fs.readFile(superInfo, 'utf8', (err, data) => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-    superInfoJSON = JSON.parse(data);
+    if(err) {
+        console.log(err);
+        return;
+    }
+    superInfoJSON = JSON.parse(data);
 })
 fs.readFile(superPowers, 'utf-8', (err, data) => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-    superPowersJSON = JSON.parse(data);
+    if(err) {
+        console.log(err);
+        return;
+    }
+    superPowersJSON = JSON.parse(data);
 });
-
+fs.readFile(lists, 'utf8', (err, data) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    listsJSON = JSON.parse(data);
+});
 
 app.use(express.json());
 
@@ -33,60 +41,122 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '..', 'client')));
 
 app.use((req, res, next) => {
-    console.log(`${req.method} request for ${req.url}`);
-    next();
+    console.log(`${req.method} request for ${req.url}`);
+    next();
 });
 
-routerInfo.route('/filter').get((req, res) => {
-        const { name, race, publisher, limit } = req.query;
-        if(!name && !race && !publisher) return res.status(400).json('There must be at least one keyword...');
-        let filteredData = superInfoJSON.filter(item =>
-            (!name || item.name.toLowerCase().includes(name.toLowerCase())) &&
-            (!race || item.Race.toLowerCase().includes(race.toLowerCase())) &&
-            (!publisher || item.Publisher.toLowerCase().includes(publisher.toLowerCase()))
-        );
-        
-        if (filteredData.length === 0) {
-            return res.status(404).json('No matching data found!');
-        }
-        if(limit > 0 && limit < filteredData.length) {
-            filteredData = filteredData.slice(0, limit);
-            console.log(filteredData)
-        }
-        res.send(filteredData);
-    })
+routerInfo.route('/lists')
+    .get((req, res) => {
+        res.send(listsJSON);
+    })
+    .post((req, res) => {
+        if(!req.body.lName) return res.status(400).send('List name blank...');
+        if(listsJSON.find(l => l.lName === req.body.lName)) return res.status(409).send('List name unavailable...');
+        listsJSON.push(req.body);
+        fs.writeFileSync(lists, JSON.stringify(listsJSON));
+        res.send(req.body);
+    })
+    
+
+routerInfo.route('/lists/:id')
+    .get((req, res) => {
+        res.send(listsJSON[req.params.id-1]);
+    })
+    .put((req, res) => {
+        console.log(listsJSON.length)
+        if(!req.body.lName) return res.status(400).send('List name blank...');
+        if(listsJSON.length === 0 || listsJSON[req.params.id-1].lName !== req.body.lName) return res.status(404).send('List not found...');
+        listsJSON[req.params.id-1] = req.body;
+        fs.writeFileSync(lists, JSON.stringify(listsJSON));
+        res.send(listsJSON);
+    })
+    .delete((req, res) => {
+        listsJSON.splice(req.params.id-1, 1);
+        fs.writeFileSync(lists, JSON.stringify(listsJSON));
+        res.send(listsJSON)
+    })
+
+routerInfo.route('/publishers')
+    .get((req, res) => {
+        const publishers = [];
+        for(const p of superInfoJSON) {
+            if(!publishers.includes(p.Publisher) && p.Publisher !== '') publishers.push(p.Publisher);
+        }
+        res.send(publishers);
+    });
+
+routerInfo.route('/filter')
+    .get((req, res) => {
+        const { name, race, publisher, power, limit } = req.query;
+        let matchingHeroes;
+        if(!name && !race && !publisher && !power) return res.status(400).json('There must be at least one keyword...');
+        else {
+            matchingHeroes = superInfoJSON.filter(item =>
+                (!name || item.name.toLowerCase().includes(name.toLowerCase())) &&
+                (!race || item.Race.toLowerCase().includes(race.toLowerCase())) &&
+                (!publisher || item.Publisher.toLowerCase().includes(publisher.toLowerCase()))
+            );
+        }
+        
+        if(power) {
+            let filteredPowers = superPowersJSON.filter(item => item[capitalize(power)] === "True");
+            const matchingHeroNames = filteredPowers.map(hero => hero.hero_names);
+            matchingHeroes = matchingHeroes.filter(hero => matchingHeroNames.includes(hero.name));
+        }
+        
+        if (matchingHeroes.length === 0) {
+            return res.status(404).json('No matching data found...');
+        }
+        if(limit > 0 && limit < matchingHeroes.length) {
+            matchingHeroes = matchingHeroes.slice(0, limit);
+        }
+        res.send(matchingHeroes);
+    });
 
 routerPower.route('/:name')
-    .get((req, res) => {
-        const name = req.params.name;
-        const powers = superPowersJSON.find(p => p.hero_names === name);
-        if(!powers) return res.status(404).send(`There are no powers found for ${name}...`);
-        res.send(powers);
-    });
+    .get((req, res) => {
+        const name = req.params.name;
+        const powers = superPowersJSON.find(p => p.hero_names === name);
+        if(!powers) return res.status(404).send(`${name} has no powers...`);
+        res.send(powers);
+    });
+
+routerInfo.route('/:id')
+    .get((req, res) => {
+        const id = req.params.id;
+        const hero = superInfoJSON.find(h => h.id === parseInt(id));
+        if(!hero) return res.status(404).send(`The superhero with ID ${id} was not found...`);
+        res.send(hero);
+    });
 
 // routerInfo.route('/search/:id')
-//     .get((req, res) => {
-//         const id = req.params.id;
-//         const hero = superInfoJSON.find(h => h.id === parseInt(id));
-//         if(!hero) return res.status(404).json(`The superhero with ID ${id} was not found...`);
-//         res.send(hero);
-//     })
+//     .get((req, res) => {
+//         const id = req.params.id;
+//         const hero = superInfoJSON.find(h => h.id === parseInt(id));
+//         if(!hero) return res.status(404).json(`The superhero with ID ${id} was not found...`);
+//         res.send(hero);
+//     })
 
 
 
 // routerInfo.route('/lists/:name')
-//     .post((req, res) => {
-//         fs.readFile(lists, 'utf-8', (err, data) => {
-//             if(err) {
-//                 res.status(500).send('Internal Server Error!');
-//                 return;
-//             }
-//             let listsJSON = JSON.parse(data);
-//         });
-//         res.send(`New list called ${req.params.name} created succesfully!`);
-//     });
+//     .post((req, res) => {
+//         fs.readFile(lists, 'utf-8', (err, data) => {
+//             if(err) {
+//                 res.status(500).send('Internal Server Error!');
+//                 return;
+//             }
+//             let listsJSON = JSON.parse(data);
+//         });
+//         res.send(`New list called ${req.params.name} created succesfully!`);
+//     });
 
 
+
+function capitalize(phrase) {
+    phrase = String(phrase).split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    return phrase;
+}
 
 app.use('/api/superheroes', routerInfo);
 app.use('/api/powers', routerPower);
