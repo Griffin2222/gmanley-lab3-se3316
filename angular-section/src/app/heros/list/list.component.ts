4bdf6295c2b21e6624a27e71754baf9abce04d5a
Index.html <gmanley@uwo.ca>
2023-11-23 00:27:56 -0500

    still need to fix populating lists

--- angular-section/src/app/heros/list/list.component.ts
+++ angular-section/src/app/heros/list/list.component.ts
@@ -1,5 +1,9 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListService } from '../../list.service';
+import { Hero } from '../../hero';
+import { HeroService } from '../../hero.service';
+import { List } from '../../list';
 
 @Component({
   selector: 'app-list',
@@ -10,12 +14,64 @@ import { CommonModule } from '@angular/common';
 })
 export class ListComponent {
   @ViewChild('listName') listNameInput!: ElementRef<HTMLInputElement>;
+  @Input() heroList: Hero[] = [];
+  lists: List[] = [];
+  selectedList: List[] = [];
 
-
-  newList(listName:string){
-    console.log('listName:',listName)
+  constructor(private listService: ListService, private heroService: HeroService){
+    this.populateDropdown();
   }
-  saveList(listName:string){
-    console.log('listName:',listName)
+
+  async createList(listN: string) {
+    try {
+      const listName = listN;
+      const idsList = this.heroList.map(hero => hero.id.toString());
+      const response = await this.listService.createList(listName, idsList);
+      console.log('List created:', response);
+      console.log(`HeroList:`, this.heroList);
+    } catch (error) {
+      console.error('Error creating list:', error);
+    }
+  }
+
+  async populateDropdown() {
+    try {
+      this.lists = await this.listService.getLists();
+      this.updateDropdown();
+    } catch (error) {
+      console.error('Error fetching lists:', error);
+    }
+  }
+
+
+  async selectList(index: number) {
+    console.log('indexc', index);
+    try {
+      const selectedLists = await this.listService.getList(index + 1);
+      if (selectedLists && Array.isArray(selectedLists) && selectedLists.length > 0) {
+        const selectedList = selectedLists[1]; // Assuming you want the first list
+        if (Array.isArray(selectedList.ids)) {
+          await this.getHeroes(selectedList.ids);
+        }
+      }
+      console.log(selectedLists);
+    } catch (error) {
+      console.error('Error selecting list:', error);
+    }
+  }
+  
+  private async getHeroes(ids: string[]) {
+    for (const id of ids) {
+      try {
+        const hero = await this.heroService.getHeroById(parseInt(id, 10)); // Assuming id is a number
+        console.log('Hero details:', hero?.name);
+        // Do something with the hero details
+      } catch (error) {
+        console.error('Error fetching hero details:', error);
+      }
+    }
+  }
+  updateDropdown(){
+
   }
 }
--- angular-section/src/app/list.service.ts
+++ angular-section/src/app/list.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { List } from './list';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
 
-  constructor() { }
+ 
+  
+
+  private getAllURL = 'http://localhost:3000/api/superheroes/lists';
+
+  //${searchName.value}&race=${searchRace.value}&publisher=${searchPublisher.value}&power=${searchPower.value}&limit=${searchLimit.value}
+
+  async createList(listName: string, idsList: string[]): Promise<any> {
+    const listItem = { listName, ids: idsList };
+    try {
+      const response = await fetch(this.getAllURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(listItem),
+      });
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getList(selectedID: number): Promise<List[] | undefined> {
+    const idNum = `/${selectedID}`;
+    const url = this.getAllURL + idNum;
+    const data = await fetch(url);
+    return (await data.json()) ?? [];
+  }
+
+  getLists(): Promise<any[]> {
+    return fetch(this.getAllURL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('Error fetching lists:', error);
+        throw error;
+      });
+  }
+
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError('Something went wrong; please try again later.');
+  }
 }
+
+
+
+
